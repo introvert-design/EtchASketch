d050b2f2269ac2f6f6ab8d552ba9282bc3dad058
GR <geo.rosy+git@protonmail.ch>
2022-02-14 14:48:00 +0530

    Add sketching, button activation and rainbow

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,8 @@
+const colorBtn = document.querySelector('.color-btn');
 const slider = document.querySelector('.slider');
 const sliderValue = document.querySelector('.slider-value');
 const sketchpad = document.querySelector('.sketchpad');
+const buttons = document.querySelectorAll('button');
 
 function displayGridValue(gridValue) {
     sliderValue.textContent = `${gridValue} x ${gridValue}`;
@@ -20,9 +22,9 @@ function drawGridLines(gridValue) {
         `grid-template-columns: repeat(${gridValue}, 1fr);`
     )
     for (let i = 0; i < gridValue ** 2; i++) {
-        let div = document.createElement('div');
+        const div = document.createElement('div');
         div.style.border = '1px solid #E8E9EB';
-        div.classList.add(`grid-${i + 1}`)
+        div.classList.add(`grid-${i}`)
         sketchpad.appendChild(div);
     }
 }
@@ -42,3 +44,56 @@ const initialGridValue = slider.value;
 configSketchpad(initialGridValue);
 
 slider.addEventListener('change', changeGrid);
+
+let isReady = false;
+let selectedOption = 'color';
+
+function selectOption(e) {
+    const previousSelectedbutton = document.querySelector(`button[value="${selectedOption}"]`);
+    previousSelectedbutton.classList.remove('active');
+    selectedOption = e.target.value;
+    e.target.classList.add('active');
+}
+
+function randomColorCode() {
+    return Math.floor(Math.random() * 256);
+}
+
+function getRandomColor() {
+    let colorCodes = [];
+    for (let i = 0; i < 3; i++) {
+        colorCodes.push(randomColorCode());
+    }
+    return `rgba(${colorCodes.join(',')})`;
+}
+
+function getColor() {
+    if (selectedOption === 'color') {
+        return;
+    }
+    else if (selectedOption === 'rainbow') {
+        return getRandomColor();
+    }
+}
+
+function sketch(e) {
+    if (isReady && e.target.className !== 'sketchpad') {
+        e.target.style.backgroundColor = `${getColor()}`;
+        // e.target.style.borderColor = 'rgba(0, 0, 0, 0.3)';
+    }    
+}
+
+function startSketch(e) {
+    isReady = true;
+    sketch(e);
+}
+
+function stopSketch() {
+    isReady = false;
+}
+
+sketchpad.addEventListener('dragstart', stopSketch);
+sketchpad.addEventListener('mouseup', stopSketch);
+sketchpad.addEventListener('mousedown', startSketch);
+sketchpad.addEventListener('mouseover', sketch);
+buttons.forEach(button => button.addEventListener('click', selectOption));
